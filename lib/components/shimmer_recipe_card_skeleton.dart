b16e3ae68820b081cf218a51846745621fb915e4
e-meyer <edmundom@mx2.unisc.br>
2022-10-24 13:53:20 -0300

    refactor: changed skeletor sizes to match the recipe_card component

--- lib/components/shimmer_recipe_card_skeleton.dart
+++ lib/components/shimmer_recipe_card_skeleton.dart
@@ -8,45 +8,42 @@ class RecipeCardSkeleton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 20.0),
-      child: SizedBox(
-        height: 420,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const Skeleton(
-                height: 345,
-                width: 345,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Skeleton(
-                height: 20,
-                width: 190,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Skeleton(
-                    height: 20,
-                    width: 110,
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Skeleton(
-                    height: 20,
-                    width: 60,
-                  ),
-                ],
-              )
-            ],
-          ),
+    return SizedBox(
+      height: 420,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Skeleton(
+              height: 345,
+              width: 345,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Skeleton(
+              height: 20,
+              width: 190,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Skeleton(
+                  height: 20,
+                  width: 110,
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Skeleton(
+                  height: 20,
+                  width: 60,
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
